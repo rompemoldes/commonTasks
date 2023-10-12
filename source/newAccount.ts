15813509aed7658bbb3ffb0577b226aff96badcc
kilted-andres <andres@trusted-entity.io>
2023-10-12 18:59:45 +0200

    feat: heavy loaded newAccount

--- source/newAccount.ts
+++ source/newAccount.ts
@@ -3,25 +3,58 @@ import { generateAccount } from "./generators/generateAccount";
 import { mnemonicGenerate } from "@polkadot/util-crypto";
 import { generateFullDid } from "./generators/generateFullDid";
 import * as Kilt from "@kiltprotocol/sdk-js";
+import { FAUCET_SEED } from "./configuration";
+import readFlags from "./flags";
 
-async function makeNewAccount(): Promise<Kilt.KiltKeyringPair> {
+makeNewAccount();
+
+async function makeNewAccount(
+  oldMnemonic?: string,
+  derivationPath?: string
+): Promise<Kilt.KiltKeyringPair> {
   const api = await getApi();
+  const flags = await readFlags();
+  // flags has priority over parameters:
+  if (flags.mnemonicTyped) {
+    oldMnemonic = flags.mnemonicTyped;
+  }
+  if (flags.derivationPath) {
+    derivationPath = flags.derivationPath;
+  }
   const chainName = (await api.rpc.system.chain()).toHuman();
   const newMnemonic = mnemonicGenerate();
-  const newAccount = generateAccount(newMnemonic);
+  const mnemonic = oldMnemonic ?? newMnemonic;
+  const baseAccount = generateAccount(mnemonic);
 
-  console.log("newMnemonic: ", newMnemonic);
-  console.log("newAccount ", newAccount.address);
+  let account = baseAccount;
+
+  if (derivationPath) {
+    account = baseAccount.derive(derivationPath) as Kilt.KiltKeyringPair;
+    console.log("derivation path: ", derivationPath);
+  }
+
+  oldMnemonic && console.log("old Mnemonic: ", mnemonic);
+  !oldMnemonic && console.log("new Mnemonic: ", mnemonic);
+
+  console.log("base Account ", baseAccount.address);
+  console.log("(possibly derived) Account ", account.address);
+
   console.log("made on this blockchain: ", chainName);
+
+  const accountBalance = (
+    await api.query.system.account(account.addressRaw)
+  ).toHuman();
+
+  console.log("account balance: ", JSON.stringify(accountBalance, null, 2));
 
   if (chainName === "KILT Peregrine") {
     const peregrineFaucetURL = "https://faucet.peregrine.kilt.io/?";
 
     console.log(
-      `get some funds for that account on the faucet here: ${peregrineFaucetURL}${newAccount.address}`
+      `get some funds for that account on the faucet here: ${peregrineFaucetURL}${account.address}`
     );
   }
 
   await api.disconnect();
-  return newAccount;
+  return account;
 }

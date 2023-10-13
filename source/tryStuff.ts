4ad49744dc0107aaf22a41ba9ddf0619ca37207a
kilted-andres <andres@trusted-entity.io>
2023-10-13 18:59:56 +0200

    feat: trying stuff

---
+++ source/tryStuff.ts
@@ -0,0 +1,40 @@
+import * as Kilt from "@kiltprotocol/sdk-js";
+
+import { ACCOUNT_MNEMONIC, DID_MNEMONIC } from "./configuration";
+import { getApi } from "./connection";
+import { generateAccount } from "./generators/generateAccount";
+import { generateFullDid } from "./generators/generateFullDid";
+import { generateKeyPairs } from "./generators/generateKeyPairs";
+import { u8aToHex } from "@polkadot/util";
+
+tryThis();
+async function tryThis() {
+  const keys = generateKeyPairs(DID_MNEMONIC);
+
+  const payer = generateAccount(ACCOUNT_MNEMONIC);
+  const didDocument = await generateFullDid(payer, DID_MNEMONIC);
+
+  console.log("The DID: ", didDocument);
+
+  console.log(
+    "keys.assertionMethod as unit8Array: ",
+    keys.assertionMethod.publicKey.toString()
+  );
+  console.log(
+    "keys.assertionMethod as hexadecimal number: ",
+    u8aToHex(keys.assertionMethod.publicKey)
+  );
+
+  const didUriFromKey = Kilt.Did.getFullDidUriFromKey(keys.authentication);
+
+  console.log("DID URI from key: ", didUriFromKey);
+
+  const isUriAsExpected = didUriFromKey === didDocument.uri;
+  console.log("Does life makes sense? ", isUriAsExpected);
+
+  const api = await getApi();
+  const chainName = (await api.rpc.system.chain()).toHuman();
+  console.log("working on this blockchain: ", chainName);
+
+  api.disconnect();
+}

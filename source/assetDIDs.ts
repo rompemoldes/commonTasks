7dae68f55680761f79079f4edf7422e00b7a73ae
kilted-andres <andres@trusted-entity.io>
2023-11-29 18:11:04 +0100

    feat: fetch credentials from asset

---
+++ source/assetDIDs.ts
@@ -0,0 +1,28 @@
+import * as Kilt from "@kiltprotocol/sdk-js";
+import { getApi } from "./connection";
+
+const cryptoKitty =
+  "did:asset:eip155:1.erc721:0x06012c8cf97bead5deae237070f9587f8e7a266d:634446";
+const cyberPunks =
+  "did:asset:eip155:1.erc721:0xb47e3cd837ddf8e4c57f05d70ab865de6e193bbb";
+const cyberPunk =
+  "did:asset:eip155:1.erc721:0xb47e3cd837ddf8e4c57f05d70ab865de6e193bbb:1005";
+
+export async function retrieveAllAssetCredentials(
+  assetDid: Kilt.AssetDidUri
+): Promise<Kilt.IPublicCredential[]> {
+  await getApi();
+  console.log("Retrieving Public Credentials regarding this asset:", assetDid);
+
+  const itsPublicCredentials =
+    await Kilt.PublicCredential.fetchCredentialsFromChain(assetDid);
+
+  for (let index = 0; index < itsPublicCredentials.length; index++) {
+    const credential = itsPublicCredentials[index];
+    console.log(`Credential #${index}`, credential);
+  }
+  return itsPublicCredentials;
+}
+retrieveAllAssetCredentials(cryptoKitty).then(() => process.exit());
+// retrieveAllAssetCredentials(cyberPunks).then(() => process.exit());
+// retrieveAllAssetCredentials(cyberPunk).then(() => process.exit());

bea558c927a1c97fac25a05e2dd1bc734f5301bc
kilted-andres <andres@kilt.io>
2024-08-26 18:19:58 +0200

    chore: todo

---
+++ source/snowBridge/getEthereumApi.ts
@@ -0,0 +1,20 @@
+// TODO:
+
+// das hier nachmachen aber ohne den context:
+// export const assetErc20Metadata = async (
+//     context: Context,
+//     tokenAddress: string
+// ): Promise<ERC20Metadata> => {
+//     const tokenMetadata = IERC20Metadata__factory.connect(tokenAddress, context.ethereum.api)
+//     const [name, symbol, decimals] = await Promise.all([
+//         tokenMetadata.name(),
+//         tokenMetadata.symbol(),
+//         tokenMetadata.decimals(),
+//     ])
+//     return { name, symbol, decimals }
+// }
+
+// Question:
+// Do we really need to add this to the environment?
+// the metadata is being fetch here again anyways:
+// https://github.com/KILTprotocol/snowbridge-app/blob/a1818fe7387c6467fd90e73e9d9f817615e83525/hooks/useSnowbridgeContext.ts#L40-L41

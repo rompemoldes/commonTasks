00b10ed6a8ced0b43d187a487f2e1e46b39ed479
kilted-andres <andres@kilt.io>
2024-08-28 18:38:47 +0200

    try: to wrapp in a promise

--- source/snowBridge/getEtherApi.ts
+++ source/snowBridge/getEtherApi.ts
@@ -21,11 +21,43 @@
 
 import { AbstractProvider, JsonRpcProvider, WebSocketProvider } from 'ethers';
 
-export function getEtherApi(wsUrl: string): AbstractProvider | undefined {
-  if (wsUrl.startsWith('http')) {
-    return new JsonRpcProvider(wsUrl);
-  }
-  if (wsUrl.startsWith('ws')) {
-    return new WebSocketProvider(wsUrl);
+export async function getEtherApi(
+  nodeUrl: string,
+): Promise<AbstractProvider | undefined> {
+  console.log('checkpoint_a');
+
+  const provider = nodeUrl.startsWith('http')
+    ? new JsonRpcProvider(nodeUrl)
+    : new WebSocketProvider(nodeUrl);
+  console.log('checkpoint_b');
+
+  try {
+    console.log('Checkpoint C: Verifying connection by fetching block number.');
+
+    if (provider instanceof WebSocketProvider) {
+      console.log('checkpoint_websocket');
+
+      await new Promise<void>((resolve, reject) => {
+        // Check if the connection is established successfully
+        provider
+          .getBlockNumber()
+          .then(() => resolve())
+          .catch((err) => reject(err));
+      });
+    } else {
+      console.log('checkpoint_http');
+
+      // For HTTP provider, a single request is sufficient
+      await provider.getBlockNumber();
+    }
+    console.log('checkpoint_d');
+
+    return provider;
+  } catch (err) {
+    console.error(
+      `Could not connect to Ethereum node at ${nodeUrl}. Reason: ${err instanceof Error ? err.message : JSON.stringify(err)}`,
+    );
+    provider.destroy();
+    return undefined;
   }
 }
